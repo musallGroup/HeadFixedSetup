092ae57b21bc1ae4a28b1ad4bb437eeba09e01a5
denlau96 <157377058+denlau96@users.noreply.github.com>
2024-09-13 18:06:03 +0200

    Connection improvements
    Serial connection is quicker now (if it is not the first session) and performance is always sent to the right server

--- Autopilot/send_perfomance.m
+++ Autopilot/send_perfomance.m
@@ -1,29 +1,51 @@
 function send_perfomance(obj, ipAddress, port)
-global BpodSystem
-    % send_perfomance Sends a MATLAB object via UDP
+% send_perfomance Sends a MATLAB object via UDP
 %     % RELEVANT DATA FOR PERFORMANCE
-% 
-%     for iMods = [1 4 5]
-%         % detection
-%         trialCnt = sum(BpodSystem.Data.Assisted & ~BpodSystem.Data.DidNotChoose & BpodSystem.Data.Modality == 1 & BpodSystem.Data.StimType == iMods);
-%         totalCnt = sum(BpodSystem.Data.Assisted & BpodSystem.Data.Modality == 1 & BpodSystem.Data.StimType == iMods);
-%         rewardCnt = sum(BpodSystem.Data.Rewarded & BpodSystem.Data.Assisted & ~BpodSystem.Data.DidNotChoose & BpodSystem.Data.Modality == 1 & BpodSystem.Data.StimType == iMods);
-%         BpodSystem.Data.PerfDet = num2str(rewardCnt/trialCnt);
-%         disp(['Self performed DETECTION ' cModality{iMods} ': ' num2str(trialCnt) ' trials (' num2str(totalCnt) ' total); Performance: ' num2str(rewardCnt/trialCnt)]);
-% 
-%         % discrimination
-%         trialCnt = sum(BpodSystem.Data.Assisted & ~BpodSystem.Data.DidNotChoose & BpodSystem.Data.Modality == 2 & BpodSystem.Data.StimType == iMods);
-%         totalCnt = sum(BpodSystem.Data.Assisted & BpodSystem.Data.Modality == 2 & BpodSystem.Data.StimType == iMods);
-%         rewardCnt = sum(BpodSystem.Data.Rewarded & BpodSystem.Data.Assisted & ~BpodSystem.Data.DidNotChoose & BpodSystem.Data.Modality == 2 & BpodSystem.Data.StimType == iMods);
-%         BpodSystem.Data.PerfDis = num2str(rewardCnt/trialCnt);
-%         disp(['Self performed DISCRIMINATION ' cModality{iMods} ': ' num2str(trialCnt) ' trials (' num2str(totalCnt) ' total); Performance: ' num2str(rewardCnt/trialCnt)]);
-%     end
+% function PuffyPenguin_checkPerformance
+%%
+modColors = [0 0 0; 0 1 0; 1 0 0.5; 1 0 0; 0.5 0.5 0; 0 0.5 1];
+modLabels = {'Vision' 'Audio' 'AudioVisual' 'Tactile' 'VisuoTactile' 'AudioTactile'};
+modIdx = [1 2 4 5]; %modalities to check (as identified by modlabels above)
+
+try %make sure this doesnt cause an error
+    cAnimal = BpodSystem.ProtocolSettings.SubjectName;
+
+    cPath = [BpodSystem.ProtocolSettings.serverPath cAnimal ...
+        '\PuffyPenguin\Session Data'];
+
+    if ~exist(cPath, 'dir')
+        error('Could not find server location for behavioral data.');
+    end
+
+    recs = dir(cPath);
+    recs = recs([recs.isdir]);
+    recs = {recs(3:end).name};
+
+    clear perf perfTrialCnt allTrialCnt
+    Cnt = 0;
+    for iRecs = 1 : length(recs)
+        try
+            cRec = [cPath filesep recs{iRecs} filesep];
+            cFile = dir([cRec '*' recs{iRecs} '.mat']);
+
+            load([cRec cFile.name]);
+            bhv = SessionData;
+            if sum(bhv.Assisted) > 25
+                Cnt = Cnt + 1;
+                for iMods = 1 : length(modIdx)
+                    allTrialCnt(iMods,Cnt) = sum(bhv.StimType == modIdx(iMods));
+                    cIdx = bhv.StimType == modIdx(iMods) & bhv.Assisted & ~bhv.DidNotChoose & bhv.Modality == 1;
+                    perf(iMods,Cnt) = sum(bhv.Rewarded(cIdx)) / sum(cIdx);
+                    perfTrialCnt(iMods,Cnt) = sum(cIdx);
+                end
+            end
+        end
+    end
+    %%
     % obj: The MATLAB object to be sent (e.g., struct, array, etc.)
     obj = '';
-    if ~isempty(BpodSystem.Data.PerfDet)
-        obj = BpodSystem.Data.PerfDet;
-    elseif ~isempty(BpodSystem.Data.PerfDis)
-        obj = BpodSystem.Data.PerfDis;
+    if ~isempty(perf)
+        obj = perf;
     else
         disp('No Performance-Data found')
     end
@@ -34,8 +56,19 @@ global BpodSystem
         expr = 'IPv4 Address[. ]+: ([\d.]+)';
     end
 
-    % Replace 'hostname' with the actual hostname of the target PC
-    hostname = 'CMP0395'; 
+    % Looks up with which Server the GUI was communicating and initiates
+    % the conncetion
+     if exist('app.hostname.mat', 'file') == 2
+        % Load the saved variable from the file
+        loadedData = load('app.hostname.mat');
+        hostname = loadedData.hostname;
+        
+        % Use the variable
+        disp(['Servername ', num2str(hostname)]);
+    else
+        disp('No Server found');
+    end
+
 
     % Use the 'ping' command to resolve the hostname to an IP address
     [status, cmdout] = system(['ping -n 1 ', hostname]);
